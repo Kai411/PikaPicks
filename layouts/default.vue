0d0efa6a37a60e60efc822d993ba061ed311d5d1
GraphicitoDev <dev@graphicito.com>
2026-05-19 15:56:35 +0800

    Added - Landing page - Edit sales listing page - Privacy Policy - Auction winner contact & profile view

---
+++ layouts/default.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="min-h-screen bg-gray-50 text-gray-900">
+    <AppNavbar class="sticky top-0" style="z-index: 2" />
+    <main class="container mx-auto px-4 py-8">
+      <slot />
+    </main>
+  </div>
+</template>
